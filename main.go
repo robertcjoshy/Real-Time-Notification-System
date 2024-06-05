d1c6959ac749ac31815a0e2bd16ce9bd877039cd
robertcjoshy <robertcjoshy11@gmail.com>
2024-06-05 23:55:09 +0530

    code

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/robert/notification/admin"
+	"github.com/robert/notification/admin/admin_handler"
+	"github.com/robert/notification/app"
 	"github.com/robert/notification/user"
+	"github.com/robert/notification/user/user_handler"
 	"github.com/robert/notification/utils"
 )
 
 func main() {
 	utils.Opendatabaseconnection()
 	router := gin.Default()
+	bolbolinstance := app.Build()
+	admin_bolbol := &admin_handler.Server{Bolbol: bolbolinstance}
+	user_bolbol := &user_handler.Server{Bolbol: bolbolinstance}
 	router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("templates/*.html")
-	user.User_routes(router)
-	admin.Admin_routes(router)
+	user.User_routes(router, user_bolbol)
+	admin.Admin_routes(router, admin_bolbol)
 	router.Run()
 }
